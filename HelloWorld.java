edf98da42606a95999638acc41cb3753e51d6191
sivajibonu <61705621+sivajibonu@users.noreply.github.com>
2020-03-11 23:08:21 +0530

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-/** 
- * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
- */
-public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Welcome to JENKINS, First program from SIVAJI");
-  }
-}
